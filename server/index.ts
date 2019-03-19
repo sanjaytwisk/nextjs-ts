ef75a8afaf2d857728cc9462683376a0c0757949
Sanjay <cmdtwisk@gmail.com>
2019-03-19 14:51:37 +0100

    Add tslint + prettier
    - Add tslint:latest, tslint-react, tslint-eslint-rules, tslint-config-prettier
    - Add lint script with autofix and nice logs

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,20 @@
-import { createServer } from 'http';
-import { parse } from 'url';
-import * as next from 'next';
+import { createServer } from 'http'
+import { parse } from 'url'
+import * as next from 'next'
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 const startApp = async () => {
-  await app.prepare();
+  await app.prepare()
   createServer((req, res) =>
     handle(req, res, parse(req.url || '', true))
   ).listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-};
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`) // tslint:disable-line
+  })
+}
 
-startApp();
+startApp()
