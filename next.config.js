370c043693f69e8ccc2ff276b152e6828db87972
Sanjay <cmdtwisk@gmail.com>
2019-03-19 14:58:28 +0100

    Clean up dependencies
    - Refactor next.config.js to only require dev dependencies when needed
    - Remove .next folder on each build

--- next.config.js
+++ next.config.js
@@ -1,51 +1,61 @@
-const withPugins = require('next-compose-plugins')
-const withTypeScript = require('@zeit/next-typescript')
-const withCSS = require('@zeit/next-css')
-const postcssPresetEnv = require('postcss-preset-env')
-const postcssNested = require('postcss-nested')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
 
-const postcssPresetEnvOptions = {
-  features: {
-    'custom-media-queries': true,
-    'custom-selectors': true,
-  },
-}
+const getBuildConfig = (...args) => {
+  const withPugins = require('next-compose-plugins')
+  const withTypeScript = require('@zeit/next-typescript')
+  const withCSS = require('@zeit/next-css')
+  const postcssPresetEnv = require('postcss-preset-env')
+  const postcssNested = require('postcss-nested')
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+  const postcssPresetEnvOptions = {
+    features: {
+      'custom-media-queries': true,
+      'custom-selectors': true,
+    },
+  }
 
-const cssOptions = {
-  postcssLoaderOptions: {
-    plugins: [postcssPresetEnv(postcssPresetEnvOptions), postcssNested()],
-  },
-}
+  const cssOptions = {
+    postcssLoaderOptions: {
+      plugins: [postcssPresetEnv(postcssPresetEnvOptions), postcssNested()],
+    },
+  }
 
-const nextConfig = {
-  webpack(config, options) {
-    if (options.isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin())
-    }
-    config.module.rules.push({
-      test: /\.svg$/,
-      include: /src\/components\/icon\/icons/,
-      use: [
-        'svg-sprite-loader',
-        {
-          loader: 'svgo-loader',
-          options: {
-            plugins: [
-              { removeAttrs: { attrs: '(fill)' } },
-              { removeTitle: true },
-              { cleanupIDs: true },
-              { removeStyleElement: true },
-            ],
+  const nextConfig = {
+    webpack(config, options) {
+      if (options.isServer) {
+        config.plugins.push(new ForkTsCheckerWebpackPlugin())
+      }
+      config.module.rules.push({
+        test: /\.svg$/,
+        include: /src\/components\/icon\/icons/,
+        use: [
+          'svg-sprite-loader',
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                { removeAttrs: { attrs: '(fill)' } },
+                { removeTitle: true },
+                { cleanupIDs: true },
+                { removeStyleElement: true },
+              ],
+            },
           },
-        },
-      ],
-    })
-    return config
-  },
+        ],
+      })
+      return config
+    },
+  }
+  return withPugins([[withTypeScript], [withCSS, cssOptions]], nextConfig)(
+    ...args
+  )
 }
 
-module.exports = withPugins(
-  [[withTypeScript], [withCSS, cssOptions]],
-  nextConfig
-)
+module.exports = (phase, ...rest) => {
+  const shouldAddBuildConfig =
+    phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD
+  return shouldAddBuildConfig ? getBuildConfig(phase, ...rest) : {}
+}
